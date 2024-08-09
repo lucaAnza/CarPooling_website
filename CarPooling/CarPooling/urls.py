4e790cc61292954151207ec23d589ee10811d3c0
Luca Anzaldi <anzaldi.luca@gmail.com>
2024-08-09 16:19:56 +0200

    Finish database er + add crispy form and some templates

--- CarPooling/CarPooling/urls.py
+++ CarPooling/CarPooling/urls.py
@@ -15,14 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include, re_path
+from django.contrib.auth import views as auth_views
 from .initcmds import init_db,erase_db
+from .views import biblio3_home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gestione/' , include('gestione.urls'))
+    path('gestione/' , include('gestione.urls')),
+    re_path(r"^$|^\/$|^home\/$", biblio3_home ,name="home") ## PROF
 ]
 
+""" WHAT????
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+"""
 
 init_db()
 #erase_db()
