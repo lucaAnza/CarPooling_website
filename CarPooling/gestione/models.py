4363943cefb28f79ec28afcefcb6644cb2dcb07d
Luca Anzaldi <anzaldi.luca@gmail.com>
2024-08-13 10:34:57 +0200

    Change on database + upgrade trip front-end

--- CarPooling/gestione/models.py
+++ CarPooling/gestione/models.py
@@ -26,16 +26,6 @@ class Car(models.Model):
         verbose_name_plural = "Cars"
 
 
-class Booking(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.PROTECT,blank=True,null=True,default=None, related_name="booking_creator")
-    car = models.ForeignKey(Car , on_delete=models.CASCADE)
-    open_registration_time = models.DateField(default=None)
-    close_registration_time = models.DateField(default=None)
-    max_passenger = models.IntegerField(default=0)
-    
-    def __str__(self):
-        return f'Booking {self.id}: User {self.user_id.id} - Car {self.car.id} - Open: {self.open_registration_time} - Close: {self.close_registration_time} - Max passengers: {self.max_passenger}'
-
 class Review(models.Model):
     rating = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(10)])
     comment = models.CharField(max_length = 100)
@@ -44,20 +34,32 @@ class Review(models.Model):
         return f'Review {self.id}: Rating {self.rating} - Comment: {self.comment}'
 
 class Ride(models.Model):
-    departure_location = models.CharField(max_length=15)
-    arrival_location = models.CharField(max_length=15)
+    car = models.ForeignKey(Car , on_delete=models.PROTECT ,null=False , related_name="my_bookings")
+    departure_location = models.CharField(max_length=30 , null  = False)
+    arrival_location = models.CharField(max_length=30 , null = False)
     departure_time = models.DateField(null=False)
     arrival_time = models.DateField(null=False)
-    max_passenger = models.IntegerField(default=0)
-
+    
     def __str__(self):
         return f'Ride {self.id}: Passenger {self.passenger_id.id} - Departure: {self.departure_location} - Arrival: {self.arrival_location} - Departure Time: {self.departure_time} - Arrival Time: {self.arrival_time} - Max passengers: {self.max_passenger}'
+
+class Booking(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.PROTECT,blank=True,null=True,default=None, related_name="my_bookings")
+    ride = models.ForeignKey(Ride , on_delete=models.PROTECT ,null=False , related_name="booking")
+    open_registration_time = models.DateField(null = False)
+    close_registration_time = models.DateField(null = False)
+    max_passenger = models.IntegerField(default=0 , null = False)
+    
+    def __str__(self):
+        return f'Booking {self.id}: User {self.user_id.id} - Car {self.car.id} - Open: {self.open_registration_time} - Close: {self.close_registration_time} - Max passengers: {self.max_passenger}'
+
 
 class Passenger(models.Model):
     ride = models.ForeignKey(Ride, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     review_id = models.ForeignKey(Review, on_delete=models.CASCADE)
 
+    # Primary Key (ride,user)
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=['ride', 'user'], name='unique_ride_user') # Composite Key
