411c842a11c04ad0138a3451364dd1ff7251991a
Luca Anzaldi <anzaldi.luca@gmail.com>
2024-08-14 16:06:03 +0200

    Changes on Database - Ride(key)

--- CarPooling/gestione/models.py
+++ CarPooling/gestione/models.py
@@ -40,15 +40,27 @@ class Ride(models.Model):
     departure_time = models.DateTimeField(null=False)
     arrival_time = models.DateTimeField(null=False)
     
+    # Primary Key (ride,user)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['id', 'car'], name='ride_primary_key') # Composite Key
+        ]
+
     def __str__(self):
         return f'Ride {self.id} - Departure: {self.departure_location} - Arrival: {self.arrival_location} - Departure Time: {self.departure_time} - Arrival Time: {self.arrival_time}'
 
 class Booking(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT,blank=True,null=True,default=None, related_name="my_bookings")
     ride = models.ForeignKey(Ride , on_delete=models.PROTECT , null=False , default = None, related_name="booking")
+    user = models.ForeignKey(User, on_delete=models.PROTECT,blank=True,null=True,default=None, related_name="my_bookings")
     open_registration_time = models.DateTimeField(null = False)
     close_registration_time = models.DateTimeField(null = False)
     max_passenger = models.IntegerField(default=0 , null = False)
+
+    # Primary Key (ride,user)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['ride', 'user'], name='booking_primary_key') # Composite Key
+        ]
     
     def __str__(self):
         return f'Booking {self.id}: User {self.user.id} - Open: {self.open_registration_time} - Close: {self.close_registration_time} - Max passengers: {self.max_passenger}'
@@ -62,7 +74,7 @@ class Passenger(models.Model):
     # Primary Key (ride,user)
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['ride', 'user'], name='unique_ride_user') # Composite Key
+            models.UniqueConstraint(fields=['ride', 'user'], name='passenger_primary_key') # Composite Key
         ]
     def __str__(self):
         return f'Passenger [ID={self.id}] : (User-{self.user.id} , Ride {self.ride.id}) ---> Review {self.review_id.id}'
